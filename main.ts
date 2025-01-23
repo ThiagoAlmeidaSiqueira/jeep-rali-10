0341f795e97f24a14f47040025c87ae335468a39
ThiagoAlmeidaSiqueira <thiagoalmeidasiqueira@gmail.com>
2025-01-23 15:11:14 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,23 +1,42 @@
+function toca_musica () {
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(740, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(880, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(740, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(659, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(1319, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(1175, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(1047, music.beat(BeatFraction.Double)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(988, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(880, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(740, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+    music.play(music.tonePlayable(784, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "X") {
         magicbit.Servo(magicbit.Servos.S1, Math.map(value, 0, 180, 45, 135))
     }
     if (name == "B1") {
         if (value == 1) {
-            magicbit.Servo(magicbit.Servos.S8, 0)
+            magicbit.MotorRun(magicbit.Motors.M1, 255)
         } else {
-            magicbit.Servo(magicbit.Servos.S8, 90)
+            magicbit.MotorRun(magicbit.Motors.M1, 0)
         }
     }
     if (name == "B3") {
         if (value == 1) {
-            magicbit.Servo(magicbit.Servos.S8, 180)
+            magicbit.MotorRun(magicbit.Motors.M1, -255)
         } else {
-            magicbit.Servo(magicbit.Servos.S8, 90)
+            magicbit.MotorRun(magicbit.Motors.M1, 0)
         }
     }
     if (name == "Z") {
-        music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
+        toca_musica()
     }
 })
 radio.setGroup(1)
